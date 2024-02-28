cf1dcac46ebab7fbd6b71bb8f2086db7370f612e
DiegoH3nr1que <diegohenrique123012@gmail.com>
2024-02-28 14:39:30 -0300

    style(): modifications in style to header

--- landing_page/src/components/Header.tsx
+++ landing_page/src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className=" flex items-center w-full h-20 bg-primary-orange">
       <div className=" w-full max-w-[1246px] px-[15px] mx-auto">
-        <div>
+        <div className=" flex flex-1 items-center justify-between">
           <div className=" flex items-center gap-14">
             <Image src={Logo} alt="Logo" />
             <ul className="flex items-center gap-12">
