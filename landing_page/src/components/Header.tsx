69f45988afee0aabda944df9fff985b4a061e726
DiegoH3nr1que <diegohenrique123012@gmail.com>
2024-02-28 16:26:33 -0300

    style(): making the header more responsive

--- landing_page/src/components/Header.tsx
+++ landing_page/src/components/Header.tsx
@@ -3,13 +3,14 @@ import Logo from "@/assets/logo.svg";
 import Image from "next/image";
 import ItemMenu from "./ItemMenu";
 import Search from "./Search";
+import IconUser from "@/assets/icon-user.svg";
 
 export default function Header() {
   return (
-    <header className=" flex items-center w-full h-20 bg-primary-orange">
-      <div className=" w-full max-w-[1246px] px-[15px] mx-auto">
+    <header className=" relative flex items-center w-full h-20 bg-primary-orange">
+      <div className=" flex items-center justify-between w-full max-w-[1246px] pl-[2%] mx-auto">
         <div className=" flex flex-1 items-center justify-between">
-          <div className=" flex items-center gap-14">
+          <div className=" flex items-center gap-20">
             <Image src={Logo} alt="Logo" />
             <ul className="flex items-center gap-12">
               <li>
@@ -25,11 +26,13 @@ export default function Header() {
                 <ItemMenu name="Ajuda" />
               </li>
             </ul>
-          </div>
-          <div>
             <Search />
           </div>
         </div>
+        <button className="flex w-[20%] justify-center items-center gap-4 bg-primary-blue h-20 pl-10">
+          <Image src={IconUser} alt="User" />
+          <span className="text-white font-bold">Acessar conta</span>
+        </button>
       </div>
     </header>
   );
--- landing_page/src/components/Search.tsx
+++ landing_page/src/components/Search.tsx
@@ -5,8 +5,12 @@ import IconSearch from "@/assets/icon-search.svg";
 export default function Search() {
   return (
     <div className=" flex items-center gap-4">
-      <Image src={IconSearch} alt="Search"/>
-      <input type="text" className=" bg-transparent outline-none text-white"/>
+      <Image src={IconSearch} alt="Search" />
+      <input
+        type="text"
+        placeholder="Buscar"
+        className=" bg-transparent outline-none pr-16 text-white placeholder:text-white"
+      />
     </div>
   );
 }
